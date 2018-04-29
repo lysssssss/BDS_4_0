ec02818dbde7bbdae286a0b13f2fc9642bdd97aa
李易燊 <jimmy591986780@qq.com>
2018-04-30 03:20:18 +0800

    ceui2

--- 北斗传输协议4.0/BD_Protocol_4_0.h
+++ 北斗传输协议4.0/BD_Protocol_4_0.h
@@ -6,13 +6,12 @@
 #include "SerialPort.h"  
 
 const int RE_BUFFER_SIZE = 2047;
-
 typedef unsigned int  UINT;
 typedef unsigned char  UCHR; 
 extern int(*myprint)(_In_z_ _Printf_format_string_ char const* const, ...);
 #define GNTX_LENTH 0x12
-//#define GNVX_LENTH 
 #define GNPX_LENTH 0x20
+//#define GNVX_LENTH 
 struct RE_BUFFER
 {
 	UINT wp;
